cb91601d9577ac02125bbda40ff791d169bcf9b6
Khanh Duong <dqkqdlot@gmail.com>
2026-06-03 21:21:43 +0900

    more snacks lsp functions

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,13 +4,17 @@ return {
   lazy = false,
   keys = {
     -- stylua: ignore start
-    { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+    { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+    { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
+    { "<leader>ff", function() Snacks.picker.files({ ignored = true }) end, desc = "Find Files" },
     { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
     { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
     { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent Files" },
     { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
     { "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
     { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
+    { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+    { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
     -- stylua: ignore end
   },
   after = function()
